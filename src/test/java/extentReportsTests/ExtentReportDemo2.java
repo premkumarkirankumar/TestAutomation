ec28eabdf15263fac1c4cae1c9a4b52c12ddf5ba
Kiran <kpremkumar@allegisgroup.com>
2019-12-19 11:27:42 -0500

    updated testng Files

--- src/test/java/extentReportsTests/ExtentReportDemo2.java
+++ src/test/java/extentReportsTests/ExtentReportDemo2.java
@@ -46,7 +46,7 @@ public class ExtentReportDemo2
 		driver=new ChromeDriver();
 		driver.get("http://www.google.com");
 		System.out.println("title is "+driver.getTitle());
-		Assert.assertTrue(driver.getTitle().contains("Mukesh"));
+		Assert.assertTrue(driver.getTitle().contains("Allegis"));
 	}
 	
         // This will run after testcase and it will capture screenshot and add in report
